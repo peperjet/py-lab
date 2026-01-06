3aca62a549c2d94c2f5f4440d91a45ba23f3adc5
peperjet <luanagashu@gmail.com>
2026-01-06 22:07:03 +0900

    인프런 집합 (6-1)

---
+++ 260106_chapter03_06.py
@@ -0,0 +1,83 @@
+# Chapter 03-6
+# 집합(Set) 특징
+# 집합 자료형(순서X, 중복X)
+
+# 선언
+a = set()
+b = set([1, 2, 3, 4, 4 , 4])
+c = set([1, 4, 5, 6])
+d = set([1, 2, 'Pen','Cap','Plate' ])
+e = {'foo', 'bar', 'baz', 'foo', 'qux' }
+f = {42, 'foo', (1,2,3), 3.14159}
+
+print('a - ', type(a), a)
+print('b - ', type(b), b)
+print('c - ', type(c), c)
+print('d - ', type(d), d)
+print('e - ', type(e), e)
+print('f - ', type(f), f)
+
+# 튜플 변환 (set -> tuple)
+t = tuple(b)
+print('t - ', type(t), t)
+print('t - ', t[0], t[1:3])
+
+#리스트 변환 (set -> list)
+l =list(c)
+l2 = list(e)
+
+print('l - ', type(l), l)
+print('l2 - ', type(l2), l2)
+
+# 길이
+print(len(a))
+print(len(b))
+print(len(c))
+print(len(d))
+print(len(e))
+print(len(f))
+
+print()
+
+# 집합 자료형 활용
+s1 = set([1, 2, 3, 4, 5, 6])
+s2 = set([4, 5, 6])
+
+print('s1 & s2 - ', s1 & s2) # 교집합
+print('s1 교집합 - ', s1.intersection(s2))
+
+print('s1 | s2 - ', s1 | s2) # 합집합
+print('s1 합집합 - ', s1.union(s2))
+
+print('s1 - s2 - ', s1 - s2) # 차집합
+print('s1 차집합 - ', s1.difference(s2))
+
+# 중복 원소 확인
+print('s1 & s2 - ', s1.isdisjoint(s2)) # 교집합이 없으면 True
+
+# 부분 집합 확인
+print('subset :', s2.issubset(s1))
+print('superset :', s1.issuperset(s2)) # s2가 s1의 슈퍼셋인가?
+
+# 추가 & 제거
+s1 = set([1, 2, 3, 4])
+
+s1.add(5)
+print('s1 - ', s1)
+
+s1.remove(2)
+print('s1 - ', s1)
+
+s1.discard(3)
+print('s1 - ', s1)
+# s1.remove(7) # 없는 값을 제거하려고 하면 에러 발생
+# s1.discard(7) # 없는 값을 제거하려고 해도 에러 발생X
+
+s1.clear()
+print('s1 - ', s1)
+
+
+a = [1, 2, 3]
+a.clear()
+print(a)
+
