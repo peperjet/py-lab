b1dfd2316ea2b5db152a73c42f0342cbae0cc3b4
peperjet <luanagashu@gmail.com>
2026-01-10 17:45:46 +0900

    [인프런] 함수(람다식 예제, input), 클래스

--- 260108_chapter05_01.py
+++ 260108_chapter05_01.py
@@ -112,3 +112,36 @@ def nested_func(num):
 nested_func(100)
 # 실행불가
 # func_in_func(1000)
+
+
+
+# 260110 람다식 예제
+# 메모리 절약, 가독성 향상, 코드 간결
+# 함수는 객체 생성 -> 리소스 (메모리) 할당
+# 람다는 즉시 실행함수(Heap 초기화) -> 메모리 초기화
+# 남발 시 가독성 오히려 감소
+
+
+# 일반적함수 -> 할당
+#def mul_func(x,y):
+#    return x * y
+
+#Lambda x, y:x*y
+
+def mul_func(x,y):
+    return x * y
+
+print(mul_func(10, 50))
+
+mul_func_var = mul_func
+print(mul_func_var(20,50))
+
+# 람다함수 -> 할당
+lambda_mul_func = lambda x,y:x*y
+print(lambda_mul_func(50,50))
+
+
+def func_final(x,y, func):
+    print('>>>>', x * y * func(100, 100))
+
+func_final(10, 20, lambda_mul_func)
